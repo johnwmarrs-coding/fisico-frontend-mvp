98aea1157f9f28bfbb6953e72f477451f989e175
johnwmarrs <johnwmarrs@tamu.edu>
2021-03-16 22:45:03 -0600

    Added Password Verification

--- fisico-app/drawers/account/screens/signupScreen.js
+++ fisico-app/drawers/account/screens/signupScreen.js
@@ -5,49 +5,57 @@ import { useContext } from 'react';
 import ThemeContext from '../../../contexts/themeContext';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { TextInput, Title, Text, Button } from 'react-native-paper';
+import { validateEmail, validatePassword} from '../../../utils/accountValidation';
 
 
 
 const SignupScreen = ( {navigation} ) => {
   const themeContext = useContext(ThemeContext);
   const [text, setText] = useState('');
+  const [emailText, setEmailText] = useState('');
+  const [displayNameText, setDisplayNameText] = useState('');
+  const [passwordText, setPasswordText] = useState('');
+  const [verifyPasswordText, setVerifyPasswordText] = useState('');
   return (
     <View style={themeContext.darkMode ? stylesDark.container : styles.container }>
       <Title style={themeContext.darkMode ? stylesDark.label : styles.label }>Welcome!</Title>
       <Text style={themeContext.darkMode ? stylesDark.paragraph : styles.paragraph }>
         Signing up lets you use social features, and save your data to the cloud!.
       </Text>
+      {emailText != '' && !validateEmail(emailText) ? <Text>Invalid Email Address</Text>: null}
       <TextInput
         style={themeContext.darkMode ? stylesDark.field : styles.field}
         label='Email'
-        onChangeText={text => setText(text)}
-        value={text}
+        onChangeText={text => setEmailText(text)}
+        value={emailText}
         mode='outlined'
       />
+      
       <TextInput
         style={themeContext.darkMode ? stylesDark.field : styles.field}
         label='Display Name'
-        onChangeText={text => setText(text)}
-        value={text}
+        onChangeText={text => setDisplayNameText(text)}
+        value={displayNameText}
         mode='outlined'
       />
+      {passwordText != '' && !validatePassword(passwordText) ? <Text>Password must be at least 6 characters long, and must contain a number, lowercase, and capital letter.</Text>: null}
       <TextInput
         style={themeContext.darkMode ? stylesDark.field : styles.field}
         label='Password'
-        onChangeText={text => setText(text)}
-        value={text}
+        onChangeText={text => setPasswordText(text)}
+        value={passwordText}
         secureTextEntry={true}
         mode='outlined'
       />
       <TextInput
         style={themeContext.darkMode ? stylesDark.field : styles.field}
         label='Verify Password'
-        onChangeText={text => setText(text)}
-        value={text}
+        onChangeText={text => setVerifyPasswordText(text)}
+        value={verifyPasswordText}
         secureTextEntry={true}
         mode='outlined'
       />
-      <Button onPress={() => setText('PRESSED')} mode='contained'>Sign Up</Button>
+      <Button onPress={() => console.log('Submit Pressed')} mode='contained'>Sign Up</Button>
       <View style={{flexDirection: 'row', justifyContent:'center', alignItems:'center'}}>
         <Text style={themeContext.darkMode ? stylesDark.paragraph : styles.paragraph }>Already a User?</Text>
         <Button mode='text' onPress={() => navigation.navigate('Signin')}>Sign In</Button>
