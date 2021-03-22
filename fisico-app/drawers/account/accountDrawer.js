a070ac2826a8f80acb243348fb955a5fd5cb8718
johnwmarrs <johnwmarrs@tamu.edu>
2021-03-22 11:47:03 -0600

    Added Account Overview Page and Logout Button

--- fisico-app/drawers/account/accountDrawer.js
+++ fisico-app/drawers/account/accountDrawer.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SigninScreen from './screens/signinScreen';
 import SignupScreen from './screens/signupScreen';
+import HomeScreen from './screens/homeScreen';
 import Header from '../../shared/header';
+import AppDataContext from '../../contexts/appDataContext';
 
 const Stack = createStackNavigator();
 const HomeTab = ( {navigation}) => {
+  const appDataContext = useContext(AppDataContext);
   return (
+    !appDataContext.loggedIn ?
     <Stack.Navigator header={Header}>
       <Stack.Screen name="Signin" component={SigninScreen} options={
         {
@@ -20,6 +24,15 @@ const HomeTab = ( {navigation}) => {
         }
       }/>
     </Stack.Navigator>
+    :
+    <Stack.Navigator header={Header}>
+      <Stack.Screen name="Signin" component={HomeScreen} options={
+        {
+          header:  () => <Header toggleDrawer={navigation.toggleDrawer} openSignin={() => navigation.navigate('Signin')} title="Account"/>
+        }
+      }/>
+    </Stack.Navigator>
+    
   )
 }
 export default HomeTab;
