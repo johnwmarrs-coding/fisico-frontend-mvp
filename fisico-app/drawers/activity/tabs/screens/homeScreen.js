140481261a80820d2b7887b78dda45008f3d6e8d
Oliver Raney <oliver@pineywoodstech.com>
2021-03-21 20:40:22 -0500

    Added dummy fetch for workout object

--- fisico-app/drawers/activity/tabs/screens/homeScreen.js
+++ fisico-app/drawers/activity/tabs/screens/homeScreen.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useContext } from 'react';
-import { ScrollView, View, StyleSheet } from 'react-native';
+import { ScrollView, View, StyleSheet, Text } from 'react-native';
 import { LightModeColors, DarkModeColors } from '../../../../styles/colors';
 import Workout from '../../workout';
 import ThemeContext from '../../../../contexts/themeContext';
 
 const HomeScreen = () => {
   const themeContext = useContext(ThemeContext);
+  const [workouts, setWorkouts] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/workout/604ec6262586462a620c3a92')
+    .then((response) => response.json())
+    .then((json) => {
+      console.log("hello");      
+      return setWorkouts(json.workout);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }, [])
+
   return (
     <ScrollView style={themeContext.darkMode ? stylesDark.container : styles.container}>
-      <Workout/>
-      <Workout/>
-      <Workout/>
-      <Workout/>
-      <Workout/>
-      <Workout/>
-      <Workout/>
-      <Workout/>
+      {/*
+      { workouts }   
+      */}
+      {workouts.map((workoutObject, index) => (
+        <Text key={index}>{ JSON.stringify(workoutObject) }</Text>
+      ))}
     </ScrollView>
   )
 }
